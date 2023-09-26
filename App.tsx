d08f0c2b7ec452a122a1b3abc8c54d9ec3108a5f
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-09-26 17:55:27 -0300

    fix: adjust imports

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 import { StatusBar, StatusBarStyle } from "react-native";
-import { ThemeProvider, useTheme } from "./src/hooks/useThemeContext";
+import { ThemeProvider } from "./src/hooks/useThemeContext";
 import { GlobalStyles } from "./src/styles/GlobalStyle";
 import Navigation from "./src/Navigation";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -11,9 +11,9 @@ export default function App() {
 
   useEffect(() => {
     async function fetchData() {
-      const savedTheme = await AsyncStorage.getItem('theme');
-      if (savedTheme === 'light') {
-        setStatusBarStyle('light-content');
+      const savedTheme = await AsyncStorage.getItem("theme");
+      if (savedTheme === "light") {
+        setStatusBarStyle("light-content");
       }
     }
 
