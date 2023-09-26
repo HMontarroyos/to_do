751dafd665c183c4b6d213b0f8619f49a737adc8
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-09-26 11:18:06 -0300

    feat: add hooks useThemeContext

---
+++ src/hooks/useThemeContext.tsx
@@ -0,0 +1,53 @@
+import React, {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+interface ThemeContextProps {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === "light" ? "dark" : "light";
+      AsyncStorage.setItem("theme", newTheme);
+      return newTheme;
+    });
+  };
+
+  useEffect(() => {
+    AsyncStorage.getItem("theme").then((savedTheme) => {
+      if (savedTheme) {
+        setTheme(savedTheme);
+      }
+    });
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = (): ThemeContextProps => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
