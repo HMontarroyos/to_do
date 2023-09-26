edb919b5dedeb2adf7a80c2ccbbe53044de1d860
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-09-26 13:38:41 -0300

    feat: add new functionalities component Task

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./styled";
 import { FontAwesome } from "@expo/vector-icons";
 import { useFonts, Teko_600SemiBold } from "@expo-google-fonts/teko";
 import { Checkbox } from "../../components";
 import { useTheme } from "../../hooks/useThemeContext";
 import { darkTheme, lightTheme, defaultTheme } from "../../styles/Theme";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface TaskProps {
   name: string;
@@ -13,10 +14,45 @@ interface TaskProps {
 
 const Task: React.FC<TaskProps> = ({ name, onDelete }) => {
   const { theme } = useTheme();
-  const [checked, setChecked] = React.useState(false);
+  const [checked, setChecked] = useState(false);
   let [fontsLoaded] = useFonts({
     Teko_600SemiBold,
   });
+
+  useEffect(() => {
+    const loadCheckedStatus = async () => {
+      try {
+        const storedStatus = await AsyncStorage.getItem(`task_checked_${name}`);
+        if (storedStatus) {
+          setChecked(storedStatus === "true");
+        }
+      } catch (error) {
+        console.error(
+          `Erro ao carregar status do Checkbox para a tarefa ${name}:`,
+          error
+        );
+      }
+    };
+
+    loadCheckedStatus();
+  }, [name]);
+
+  const toggleChecked = async () => {
+    const newStatus = !checked;
+    setChecked(newStatus);
+
+    try {
+      await AsyncStorage.setItem(
+        `task_checked_${name}`,
+        JSON.stringify(newStatus)
+      );
+    } catch (error) {
+      console.error(
+        `Erro ao salvar status do Checkbox para a tarefa ${name}:`,
+        error
+      );
+    }
+  };
 
   if (!fontsLoaded) {
     return null;
@@ -24,7 +60,7 @@ const Task: React.FC<TaskProps> = ({ name, onDelete }) => {
 
   return (
     <S.TaskContainer>
-      <Checkbox checked={checked} onPress={() => setChecked(!checked)} />
+      <Checkbox checked={checked} onPress={toggleChecked} />
       <S.TaskText
         style={{
           fontFamily: defaultTheme.fonts.paragraph,
